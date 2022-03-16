e3e423eb92333ada2976baf6196d6fb6a5f042fd
John Fuhrmeister <61069716+JohnFuhrm12@users.noreply.github.com>
2022-03-16 19:28:42 -0400

    Add files via upload

--- djangowebapp/settings.py
+++ djangowebapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,6 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-a7c716qh!94n#%4qo5mvnot(^ow#u)%dve^o-csdr(t8^i1-_b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -29,7 +33,6 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello',
--- djangowebapp/urls.py
+++ djangowebapp/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from hello.views import index
+from hello import views
 
 urlpatterns = [
-    path('hello/', index)
+    path('hello/', views.index)
 ]
 
