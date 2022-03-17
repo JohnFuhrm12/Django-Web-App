b2bac0dfde21addf8598a0cdac9ee01a4b40dc42
John Fuhrmeister <61069716+JohnFuhrm12@users.noreply.github.com>
2022-03-17 19:04:44 -0400

    Add files via upload

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from .models import Post
+
+posts = [
+      {
+        'author': 'JohnFuhrm',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'August 27, 2021'
+      },
+      {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'August 28, 2021'
+      }
+]
 
 def index(request):
   return render(request, 'index.html')
@@ -7,4 +23,7 @@ def page(request):
   return render(request, 'page.html')
 
 def test(request):
-  return render(request, 'testpage.html')
+  context = {
+    'posts': posts
+  }
+  return render(request, 'testpage.html', context)
