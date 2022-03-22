e7df11c36301543357743b956347f0c8a2bd36ff
John Fuhrmeister <61069716+JohnFuhrm12@users.noreply.github.com>
2022-03-22 00:05:31 -0400

    Add files via upload

--- hello/views.py
+++ hello/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from .models import Post
 
 def index(request):
-  return render(request, 'index.html')
+    return render(request, 'index.html')
 
 def page(request):
-  return render(request, 'page.html')
+    return render(request, 'page.html')
 
 def test(request):
-  context = {
-    'posts': Post.objects.all()
-  }
-  return render(request, 'testpage.html', context)
+    context = {
+      'posts': Post.objects.all()
+    }
+    return render(request, 'testpage.html', context)
