dd26a8ffee493384da12ce6260d79cd2950b7e56
John Fuhrmeister <61069716+JohnFuhrm12@users.noreply.github.com>
2022-03-22 00:06:47 -0400

    Add files via upload

--- djangowebapp/settings.py
+++ djangowebapp/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello',
+    'hello.apps.HelloConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
--- djangowebapp/urls.py
+++ djangowebapp/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from hello import views
+from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.index),
+    path('home', views.index, name='home'),
     path('admin/', admin.site.urls),
     path('about', views.page),
-    path('test', views.test)
+    path('test', views.test),
+    path('register/', user_views.register),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
 
